9f9afe98ef07dbd464b7af6938c40397dc0e1ee6
mln9 <93353868+mln9@users.noreply.github.com>
2022-03-23 20:35:37 +0300

    Add files via upload

---
+++ BARS GROUP/firstprogramBARS/firstprogramBARS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace firstprogramBARS
+{
+
+    public class First
+    {
+        public event EventHandler<char> OnKeyPressed;
+        public void Run()
+        {
+             var btn = Console.ReadKey();
+            while(true)
+                {
+                if (btn.Key == ConsoleKey.C)
+                {
+                    Console.WriteLine("\n Завершение работы");
+                    break;
+                }
+                else
+                {
+                    OnKeyPressed?.Invoke(this, btn.KeyChar);
+                }
+            }      
+           
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = new First();
+            key.OnKeyPressed += (Sender, Key) => Console.Write("\n" + Key + " ");
+            key.Run();
+        }
+    }
+}
