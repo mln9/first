3c88db34600de0a52da3c004f362d7494b7e3716
mln9 <93353868+mln9@users.noreply.github.com>
2022-03-30 15:03:44 +0300

    Add files via upload

---
+++ Logger/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception ex);
+    }
+    public class LocalFileLogger<T> : ILogger
+    {
+        string way;
+        private string GenericTypeName => typeof(T).Name;
+
+        public LocalFileLogger(string way)
+        {
+            this.way = way;
+        }
+        public void LogInfo(string message)
+        {
+            Write($"[Info]: [{GenericTypeName}] : {message}");
+        }
+        public void LogWarning(string message)
+        {
+            Write($"[Warning] : [{GenericTypeName}] : {message}");
+        }
+        public void LogError(string message, Exception ex)
+        {
+            Write($"[Info]: [{GenericTypeName}] : {message}");
+        }
+        private void Write(string message)
+        {
+            using (StreamWriter w = new StreamWriter(way, true))
+            {
+                w.WriteLine(message);
+
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LocalFileLogger<string> first = new LocalFileLogger<string>("first.txt");
+            //LocalFileLogger<int> second = new LocalFileLogger<int>("second.txt");
+            List<ILogger> loggers = new List<ILogger>()
+            {
+                 new LocalFileLogger<string>("first.txt"),
+                 new LocalFileLogger<int>("second.txt")
+            };
+            foreach (var item in loggers)
+            {
+                item.LogError("gkgkg",new Exception ("test"));
+
+            }
+        }
+    }
+}
